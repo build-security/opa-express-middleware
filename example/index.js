42dd5c336949023c799c3e2345298360c60c97b9
Amir Ben Nun <amir@build.security>
2020-10-26 22:02:00 +0200

    timeout

--- example/index.js
+++ example/index.js
@@ -5,19 +5,17 @@ const extAuthz = require('opa-express-middleware');
 const app = express();
 const jsonParserMiddleware = bodyParser.json();
 const extAuthzMiddleware = extAuthz((req) => ({
-    includeBody: true,
-    includeHeaders: true,
     authzServer: "http://localhost:8181/v1/data/amir",
     filter: req.method === "GET",
-    enrich: { serviceID: 1 }
+    enrich: { serviceId: 1 }
 }));
 app.use(jsonParserMiddleware, extAuthzMiddleware);
 
 app.all('/', (req, res) => {
-    res.send("amir")
+    res.send("allowed");
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 })
