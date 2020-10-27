85f589b523190bb963d1da9d68411518319fba10
Amir Ben Nun <amir@build.security>
2020-10-27 10:48:33 +0200

    add eslint

--- example/index.js
+++ example/index.js
@@ -5,17 +5,17 @@ const extAuthz = require('opa-express-middleware');
 const app = express();
 const jsonParserMiddleware = bodyParser.json();
 const extAuthzMiddleware = extAuthz((req) => ({
-    authzServer: "http://localhost:8181/v1/data/amir",
-    filter: req.method === "GET",
-    enrich: { serviceId: 1 }
+    authzServer: 'http://localhost:8181/v1/data/amir',
+    filter: req.method === 'GET',
+    enrich: {serviceId: 1},
 }));
 app.use(jsonParserMiddleware, extAuthzMiddleware);
 
 app.all('/', (req, res) => {
-    res.send("allowed");
+    res.send('allowed');
 });
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-})
+});
